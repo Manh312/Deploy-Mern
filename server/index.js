0a9096104dc0d8a0feb417d19c922d50ac306b42
Manh312 <138138781+Manh312@users.noreply.github.com>
2024-10-03 17:57:06 +0700

    Update index.js

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ const cookieParser = require('cookie-parser');
 const app = express();
 app.use(express.json());
 app.use(cors({
-  origin: [''], 
+  origin: ['https://deploy-mern-login-registration.vercel.app'], 
   methods: ['GET', 'POST'],
   credentials: true 
 }));
