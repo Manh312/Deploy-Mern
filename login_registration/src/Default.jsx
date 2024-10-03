f8407e96c3e41abe6bad6ee67942ee0c7622108c
Manh312 <138138781+Manh312@users.noreply.github.com>
2024-10-03 19:57:18 +0700

    Update Default.jsx

--- login_registration/src/Default.jsx
+++ login_registration/src/Default.jsx
@@ -1,1 +1,19 @@
+import { useEffect } from 'react';
+import axios from 'axios';
 
+const Default = () => {
+  axios.defaults.withCredentials = true;
+
+  useEffect(() => {
+    axios.get('https://deploy-mern-api-beta.vercel.app')
+    .then(result => console.log(result))
+    .catch(err => console.log(err));
+  }, []);
+  return (
+    <div>
+      <h1 className='text-center'>ABC</h1>
+    </div>
+  );
+}
+
+export default Default;
