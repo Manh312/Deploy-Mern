569c13d4219abeb49d57f48d4a52ffeba4df8321
Manh312 <138138781+Manh312@users.noreply.github.com>
2024-10-03 19:54:45 +0700

    Update App.jsx

--- login_registration/src/App.jsx
+++ login_registration/src/App.jsx
@@ -12,7 +12,7 @@ function App() {
       <Routes>
         <Route path='/register' element={<SignUp />} />
         <Route path='/login' element={<Login />} />
-        <Route path='/home' element={<Home />} />
+        <Route path='/' element={<Home />} />
       </Routes>
     </BrowserRouter>
   )
