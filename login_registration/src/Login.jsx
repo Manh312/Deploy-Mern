aa7b3956c024648cfa046c01475561992c061612
Manh312 <138138781+Manh312@users.noreply.github.com>
2024-10-03 17:33:15 +0700

    Update Login.jsx

--- login_registration/src/Login.jsx
+++ login_registration/src/Login.jsx
@@ -10,7 +10,7 @@ const Login = () => {
   axios.defaults.withCredentials = true;
   const handleSubmit = (event) => {
     event.preventDefault();
-    axios.post('http://localhost:3000/login', { email, password})
+    axios.post('http://deploy-mern-api.vercel.app/login', { email, password})
     .then(result => {
       console.log(result);
       if (result.data === 'Success') {
