0c901762a83b50e0931bd869c2c8a61c372337d3
Manh312 <138138781+Manh312@users.noreply.github.com>
2024-10-03 17:36:02 +0700

    Update SignUp.jsx

--- login_registration/src/SignUp.jsx
+++ login_registration/src/SignUp.jsx
@@ -10,7 +10,7 @@ const SignUp = () => {
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    axios.post('http://deploy-mern-api.vercel.app/register', {name, email, password})
+    axios.post('deploy-mern-api-jade.vercel.app/register', {name, email, password})
     .then(result => {console.log(result)
       navigate('/login');
     })
